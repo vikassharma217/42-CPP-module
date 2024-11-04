daf4b86cba1739adeb21ba05a2e6639147a1bf5e
vikas <vikassharma217@gmail.com>
2024-11-04 10:46:16 +0100

    CPP08 ex00

--- CPP08/ex00/easyfind.hpp
+++ CPP08/ex00/easyfind.hpp
@@ -1,7 +1,16 @@
 #ifndef EASYFIND_HPP
-#define EASYFIND_HPP
+# define EASYFIND_HPP
 
+# include <iostream> // for std::find
+# include <algorithm> // for iterator defination
+# include <iterator>
+# include <exception>
+# include <stdexcept>
+
+/*tempalte declaration*/
 template <typename T>
-void easyfind(T param1, int param2);
+typename T::iterator  easyfind(T& param1, int param2);
+
+# include "easyfind.tpp"
 
 #endif
---
+++ CPP08/ex00/easyfind.tpp
@@ -0,0 +1,12 @@
+# include "easyfind.hpp"
+
+template <typename T>
+typename T::iterator easyfind (T& param1, int param2)
+{
+    typename T::iterator it = std::find(param1.begin(), param1.end(), param2);
+    if (it == param1.end())
+    {
+        throw std::runtime_error("Value is not found.");
+    }
+    return it;
+}
